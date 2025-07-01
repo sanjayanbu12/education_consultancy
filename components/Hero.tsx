231e63ee8a75f4e6c6ffb6039d9eaa47fabe0c2f
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2025-07-01 17:57:53 +0530

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/style";
-import { discount, robot } from "@/public/assets";
+import { discount, books } from "@/public/assets";
 // import GetStarted from "./GetStarted";
 import Image from "next/image";
 
@@ -8,34 +8,25 @@ const Hero: React.FC = () => (
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
-      <div className=" flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-        <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph} ml-2`}>
-          <span className="text-white">20%</span> Discount For{" "}
-          <span className="text-white">1 Month</span> Account
-        </p>
-      </div>
+      
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-          The Next <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span>{" "}
+          Build Your
+         <br className="sm:block hidden" />{" "}
+          <span className="text-gradient"> Career With Smarter</span>{" "}
         </h1>
-        {/* <div className="ss:flex hidden md:mr-4 mr-0">
-          <GetStarted />
-        </div> */}
+        
       </div>
       <h1 className="w-full font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-        Payment Methond.
+        Choices.
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Our team of experts use a methodology to identify the credit cards most
-        likely to fit your needs. We examine annual percentage rates, annual
-        fees.
+       Finishing school is only the start. With expert support, career mapping, and global education guidance, we bridge the gap between classroom learning and real-world success.
       </p>
     </div>
     <div className={`${styles.flexCenter} flex-1 flex md:my-0 my-10 relative`}>
       <Image
-        src={robot}
+        src={books}
         alt="billings"
         className="w-[100%] h-[100%] relative z-[5]"
       />
