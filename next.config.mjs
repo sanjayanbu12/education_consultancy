dec27904eb7e93398d5143061d7326483a9b879a
Sanjay Anbazhagan <134280780+sanjayanbu12@users.noreply.github.com>
2025-12-06 17:16:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  
+  // output: "export",  // Commented out to support dynamic routes in development  
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
+
   eslint: {
     ignoreDuringBuilds: true,
   },
